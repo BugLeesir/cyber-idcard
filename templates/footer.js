1e82814529f60c252eb766c42a1257988c00994f
Glitch (glitch-in-bio) <none>
2024-05-10 06:56:58 +0000

    🍽🐟 Checkpoint ./templates/footer.js:66299569/202

--- templates/footer.js
+++ templates/footer.js
@@ -3,8 +3,8 @@ import settings from "../settings.json";
 
 export const renderFooter = html.node`<footer class="footer">
   <div class="tagline">
-    <b>Glitch in Bio</b><br />
-    Your links, your way.
+    <b>^V^</b><br />
+    Codeing everyday 
   </div>
   <a
     class="btn--remix"
